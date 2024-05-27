8062a2a81b8dcf7c9075774237f8622f36cf36b8
Marcelo Navarro <marcelo.nd.mx@gmail.com>
2024-05-27 17:49:04 +0200

    Increased pallette generation size

--- graphs.R
+++ graphs.R
@@ -8,17 +8,17 @@ if (!require("tidyr", quietly = TRUE))
 if (!require("dplyr", quietly = TRUE))
   install.packages("dplyr")
 
-get_palette <- function(nColors = 50){
+get_palette <- function(nColors = 60){
   return(c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442",
                            "#0072B2","brown1", "#CC79A7", "olivedrab3", "rosybrown",
                            "darkorange3", "blueviolet", "darkolivegreen4", "lightskyblue4", "navajowhite4",
                            "purple4", "springgreen4", "firebrick3", "gold3", "cyan3",
-                           "plum", "mediumspringgreen", "blue", "yellow", "#053f73", "lavenderblush4", "lawngreen", "indianred1", "lightblue1", "honeydrew4", "hotpink",
+                           "plum", "mediumspringgreen", "blue", "yellow", "#053f73", "lavenderblush4", "lawngreen", "indianred1", "lightblue1", "honeydew4", "hotpink",
                            "#e3ae78", "#a23f3f", "#290f76", "#ce7e00", "#386857",
-                           "#738564", "#e89d56", "#cd541d", "#1a3a46", "#ffe599",
-                           "#583E26", "#A78B71", "#F7C815", "#EC9704", "#9C4A1A",
-                           "firebrick2", "#C8D2D1", "#14471E", "#EE9B01", "#DA6A00",
-                           "#4B1E19", "#C0587E", "#FC8B5E", "#EA592A", "#FEF4C0", "khaki3", "lavenderblush3", "indianred4", "lightblue", "honeydrew1", "hotpink4", "ivory3")[1:nColors])
+                           "#738564", "#e89d56", "#cd541d", "#1a3a46", "#9C4A1A", "#ffe599",
+                           "#583E26", "#A78B71", "#F7C815", "#EC9704", 
+                           "#4B1E19", "firebrick2", "#C8D2D1", "#14471E", "#EE9B01", "#DA6A00",
+                           "#C0587E", "#FC8B5E", "#FEF4C0", "#EA592A", "khaki3", "lavenderblush3", "indianred4", "lightblue", "honeydew1", "hotpink4", "ivory3")[1:nColors])
   }
 
 barplot_from_feature_table <- function(feature_table){
