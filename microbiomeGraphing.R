50b7477373aa2d9bfca376a14cddf945629b33f8
Marcelo Navarro <marcelo.nd.mx@gmail.com>
2024-05-29 11:16:57 +0200

    Deleted prevous version of barplot_from_feature_tables
    New version allows to do graphs from prevouos version and new version.

--- microbiomeGraphing.R
+++ microbiomeGraphing.R
@@ -66,71 +66,8 @@ barplot_from_feature_table <- function(feature_table){
   return(otu_barplot)
 }
 
-barplot_from_feature_tables <- function(feature_tables, experiments_names){
-  # each experiment should have a separate table and should share samples and species
-  for (table in seq(from = 1, to = length(feature_tables), by=1)) {
-    #print(head(feature_tables))
-    # copy feature table
-    feature_table2 <- feature_tables[[table]]
-    
-    #print(head(feature_table2))
-    
-    # Remove columns (samples) with zero count
-    if (ncol(feature_table2) > 1) {
-      feature_table2 <- feature_table2[, colSums(feature_table2 != 0) > 0]
-    }
-    
-    # Generate a column with the names of ASVs/OTUs using rownames.
-    feature_table2["species"] <- row.names(feature_table2)
-    
-    feature_table_g <- tidyr::gather(feature_table2, 1:(ncol(feature_table2) - 1) , key = "sample", value = "abundance")
-    
-    print(experiments_names[table])
-    
-    feature_table_g$experiment <- experiments_names[table]
-    
-    if (table == 1) {
-      exp_plot_table <- feature_table_g
-    }else{
-      exp_plot_table <- rbind(exp_plot_table, feature_table_g)
-    }
-  }
-  
-  #print(exp_plot_table)
-  
-  # Keep order of samples in graph
-  exp_plot_table$sample <- factor(exp_plot_table$sample, levels = colnames(feature_table2))
-  
-  # 
-  color_palette <- get_palette()
 
-  exp_plot_table$experiment <- factor(exp_plot_table$experiment,levels=unique(as.character(exp_plot_table$experiment)))
-  #exp_plot_table$species <- factor(exp_plot_table$species,levels = unique(as.character(exp_plot_table$species)))
-  
-  exp_plot_table <- exp_plot_table %>%
-    dplyr::arrange(species)
-  
-  # Reorder factor
-  #exp_plot_table$species <- forcats::fct_relevel(exp_plot_table$species, after = 0)
-  #exp_plot_table$species <- forcats::fct_rev(exp_plot_table$species)
-  
-  #print(exp_plot_table)
-  
-  otu_barplot <- ggplot(exp_plot_table) +
-    geom_bar(aes(x = experiment, y = abundance, fill = species),
-             position = position_fill(),
-             stat = "identity") + 
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1)) +
-    ggplot2::scale_fill_manual(values=color_palette) +
-    ggplot2::theme(plot.title = ggplot2::element_text(size = 12, face = "bold"),
-                   legend.title=ggplot2::element_text(size=14), 
-                   legend.text=ggplot2::element_text(size=12)) +
-    facet_grid(~sample)
-  otu_barplot
-  return(otu_barplot)
-}
-
-barplot_from_feature_tables2 <- function(feature_tables, experiments_names, shared_samples = FALSE){
+barplot_from_feature_tables <- function(feature_tables, experiments_names, shared_samples = FALSE){
   # Create a barplot from several otu tables. This otutables can correspond to different experiments, runs or treatments.
   # If treatments are used, it can graph samples in a different pane per treatment where each treatment should share the samples.
   # If only different Samples types are to be graphed, samples per treatment can correspond to replicates.
